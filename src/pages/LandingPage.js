6856bb7267236aea5ffa3a6f14f64d43ca367eb8
Ilham zaki <ilhamzaki32@gmail.com>
2022-02-07 22:13:42 +0700

    Create parts Testimony and Footer

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -6,6 +6,8 @@ import MostPicked from "parts/MostPicked";
 
 import dataLandingPage from "json/landingPage.json";
 import Categories from "parts/Categories";
+import Testimony from "parts/Testimony";
+import Footer from "parts/Footer";
 
 export default class LandingPage extends Component {
   constructor(props) {
@@ -22,6 +24,8 @@ export default class LandingPage extends Component {
           data={dataLandingPage.mostPicked}
         />
         <Categories data={dataLandingPage.categories} />
+        <Testimony data={dataLandingPage.testimonial} />
+        <Footer />
       </>
     );
   }
